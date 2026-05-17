8a974fae12414a850c9c5778ac5b3437187a5de3
Vallesty <mike.vallesty@gmail.com>
2026-05-17 10:02:22 +0700

    update

--- api/proxy-curl.php
+++ api/proxy-curl.php
@@ -9,29 +9,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    die('Method Not Allowed');
-}
-
 $data = json_decode(file_get_contents('php://input'), true);
 
 $url = $data['url'] ?? null;
 
 if (!$url) {
     http_response_code(400);
-    die('Please inform URL');
+    die('Missing URL');
 }
 
-$agent = 'Mozilla/5.0';
+$ch = curl_init();
 
-$ch = curl_init($url);
+curl_setopt_array($ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_SSL_VERIFYPEER => false,
 
-curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    CURLOPT_HTTPHEADER => [
+        'Accept: application/json',
+        'Accept-Language: en-US,en;q=0.9',
+    ],
 
-$html = curl_exec($ch);
+    CURLOPT_USERAGENT =>
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+]);
+
+$response = curl_exec($ch);
 
 if (curl_errno($ch)) {
     http_response_code(500);
@@ -41,6 +45,6 @@ if (curl_errno($ch)) {
 
 curl_close($ch);
 
-header('Content-Type: text/html');
+header('Content-Type: application/json');
 
-echo $html;
+echo $response;
